5591154e0064cf0ebc74d005d17b19ca660e133c
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-07 17:42:47 -0300

    add end point

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/',function(peticion,respuesta){
-    respuesta.send("hola brocoli");
+    respuesta.send(200, {productos : lista})
 });
 
-app.listen(3000,function(peticion, respuesta){
-    console.log("hola mundo");
+app.listen(3000, () =>{
+    console.log("andandooo")
 });
  
 
+const lista = {
+    "producto": "pantalon",
+    "precio" : 20,
+}
