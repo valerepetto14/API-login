c800ccfb8a9b880a040bd225e0ea9ed5e0b72a36
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-07 01:33:41 -0300

    add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',function(peticion,respuesta){
-    respuesta.send("hola mundo");
+    respuesta.send("hola santiago y mateo");
 });
 
 app.listen(3000,function(peticion, respuesta){
