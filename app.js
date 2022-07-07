46fe23026c148d17e45500cccbfc05f2023d283f
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-07 01:44:50 -0300

    add msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',function(peticion,respuesta){
-    respuesta.send("hola santiago y mateo");
+    respuesta.send("hola brocoli");
 });
 
 app.listen(3000,function(peticion, respuesta){
