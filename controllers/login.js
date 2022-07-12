3c3496cb056a22a547cce30b952e2e911f64a7ed
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-12 14:31:02 -0300

    add login

--- controllers/login.js
+++ controllers/login.js
@@ -2,16 +2,25 @@ const conexion = require('../db')
 const bcrypt = require('bcrypt');
 
 const login = async (req, res) =>{
-    const { body }= req
-    const { user, pass } = body
+    let { body }= req
+    let { user, pass } = body
     // const passcrypt = await bcrypt.hash(pass, 8)
     conexion.query('SELECT user, pass FROM usuarios WHERE ?',{user:user},(error,results)=>{
         if (error){
             throw error;
         }else{
             // const data = JSON.parse(results)
-            const password = results.pass
-            res.send(password)
+            if(results.length){
+                res.send("no estas registrado/a")
+            }else{
+                let passBD = result[0].pass
+                let compare = bcrypt.compare(pass,passBD)
+                if (compare){
+                    res.send("Acceso exitoso")
+                }else{
+                    res.send("Una de tus credenciales fallo")
+                }
+            }
         }
     });
 }
