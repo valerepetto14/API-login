c622ef1980868a5e6889fb3165fc11d8c0395674
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-13 20:11:29 -0300

    add throw error

--- controllers/updatePass.js
+++ controllers/updatePass.js
@@ -16,11 +16,15 @@ const updatePass = (req, res) => {
                 error: "contraseña equivocada"
             })
         }else{
-            conexion.query('UPDATE usuarios SET pass = ? WHERE user = ?',{newpass, user}, (error,results)=>{
-                res.status(200).json({
-                    state: "ok",
-                    user: user
-                })
+            conexion.query('UPDATE usuario SET pass = ? WHERE user = ?',{newpass, user}, (error,results)=>{
+                if (error){
+                    throw error
+                }else{
+                    res.status(200).json({
+                        state: "ok",
+                        user: user
+                    })
+                }
             }) 
         }   
     })
