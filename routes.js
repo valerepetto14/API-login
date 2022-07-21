0da88cff5eb50d1be267ac45c49b0d053fc72302
valentin repetto <valentinrepetto12345@gmail.com>
2022-07-21 13:39:19 -0300

    add changes en tasks

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const conexion = require('./db')
 const register = require('./controllers/register')
 const login = require('./controllers/login')
 const updatePass = require('./controllers/updatePass')
+const addTasks = require('./controllers/addTasks')
 
 router.get('/api/usuarios', (req,res) => {
     if (req.user.rol !== "admin"){
@@ -31,7 +32,23 @@ router.get('/api/admin', (req, res)=>{
     }
 })
 
+router.get('/tasks', (req, res) =>{
+    const rol = req.user.rol
+    if(rol !== "admim"){
+        res.status(401).json({error: "acceso denegado, no sos admin"})
+    }else{
+        conexion.query('SELECT * FROM tasks;', (error,results)=>{
+            if (error){
+                throw error;
+            }else{
+                res.json(results)
+            }
+        })
+    }
+});
+
 router.post('/register',register.regis)
 router.post('/login',login.login)
 router.post('/api/updatepass', updatePass.updatePass)
+router.post('/api/addtasks',addTasks.addTasks)
 module.exports = router;
