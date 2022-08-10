f4dcf38c4c6f9cb4730566dd1cfa5bb288b93575
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-10 22:25:02 +0000

    add docker file

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,7 @@ const login = async (req, res) =>{
     let { body }= req
     let { user, pass } = body
     // const passcrypt = await bcrypt.hash(pass, 8)
-    conexion.query('SELECT id, user, rol,pass FROM usuarios WHERE ?',{user:user},async (error,results)=>{
+    conexion.query('SELECT id, user, rol,pass FROM usuarios WHERE ?',{user:user}, async (error,results)=>{
         if (error){
             throw error;
         }else{
